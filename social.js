f48052b88668c9bbe566cdbf46926a761bd5c0fa
Javier Garcia <javiergarcia7991@gmail.com>
2023-02-09 23:28:47 -0800

    Passing Part 1

--- social.js
+++ social.js
@@ -9,22 +9,56 @@ class SocialNetwork {
 
   addUser(name) {
     // Your code here
+    this.currentID++;
+
+    let newUser = {
+      "id": this.currentID,
+      "name": name
+    };
+
+    this.users[newUser.id] = newUser;
+    this.follows[newUser.id] = new Set();
+
+    return newUser.id;
   }
 
   getUser(userID) {
     // Your code here
+
+    if (!this.users[userID]) return null;
+
+    return this.users[userID];
   }
 
   follow(userID1, userID2) {
     // Your code here
+
+    if (!this.users[userID1] || !this.users[userID2]) return false;
+    if (this.follows[userID1].has(userID2)) return false;
+
+    this.follows[userID1].add(userID2);
+    return true;
   }
 
   getFollows(userID) {
     // Your code here
+    return this.follows[userID];
   }
 
   getFollowers(userID) {
-    // Your code here
+   // Your code here
+    let followers = new Set();
+
+    for (let user in this.follows) {
+      let userFollows = this.getFollows(user);
+
+      if (userFollows.has(userID)) {
+        let userID = this.getUser(user).id;
+        followers.add(userID);
+      }
+    }
+
+    return followers;
   }
 
   getRecommendedFollows(userID, degrees) {
